bba2e83e6a734164374f73c0ac9050220409f51c
Josip Skrlec <jskrlec1997@gmail.com>
2018-10-25 08:33:57 +0200

    Projekt u kojem koristimo leap Motion koji prepozna polozaj ruke te iscrtava 3D linije

---
+++ LeapMotionExtend/Assets/trigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour {
+
+    public GameObject loptica;
+    public GameObject kockica;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == "loptica")
+        {
+            Destroy(loptica);
+        }
+    }
+
+
+
+
+}
